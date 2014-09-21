145c703c241634ab7e923da1a0ce333106763925
Anorov <anorov.vorona@gmail.com>
2014-09-21 19:32:17 -0400

    Convert to requests adapter

--- README.md
+++ README.md
@@ -1,25 +1,56 @@
 cloudflare-scrape
 =================
 
-A simple Python function to bypass Cloudflare's anti-bot page. They change their method periodically, so I will update this repo frequently.
+A simple Python module to bypass Cloudflare's anti-bot page, implemented as a [Requests](https://github.com/kennethreitz/requests) adapter. Cloudflare changes their techniques periodically, so I will update this repo frequently.
 
-This can be useful if you wish to scrape or crawl a website protected with Cloudflare. You can also expand the function to return the `Session` object (`sess` in this case) and use it to crawl the site further.
+This can be useful if you wish to scrape or crawl a website protected with Cloudflare. Cloudflare's anti-bot page currently just checks if the client supports Javascript, though they may add additional techniques in the future.
 
 Due to Cloudflare continuously changing and hardening their protection page, `cloudflare-scrape` now uses **PyV8**, a wrapper around Google's V8 Javascript engine.
 
-Note: This only works when regular Cloudflare anti-bots is enabled (the "please wait 5 seconds..." loading page). If there is a reCAPTCHA challenge, you're out of luck.
+Note: This only works when regular Cloudflare anti-bots is enabled (the "Checking your browser before accessing..." loading page). If there is a reCAPTCHA challenge, you're out of luck. Thankfully, the Javascript check page is much more common.
+
+For reference, this is the default message Cloudflare uses for these sorts of pages:
+
+    Checking your browser before accessing website.com.
+
+    This process is automatic. Your browser will redirect to your requested content shortly.
+
+    Please allow up to 5 seconds…
 
 Dependencies
 ============
 * Python 2.6 - 2.7
-* **[requests](https://github.com/kennethreitz/requests)** >= 2.0
+* **[Requests](https://github.com/kennethreitz/requests)** >= 2.0
 * **[PyV8](https://code.google.com/p/pyv8/)**
 
 There are a few different ways to install PyV8, depending on your OS and if you want to compile it from source or use a pre-compiled binary. Use whatever works best for you.
 
 Usage
 ====
-    from cfscrape import grab_cloudflare
+The simplest way to use cfscrape is by calling `create_scraper()`.
 
-    url = "http://somesite.com"
-    print grab_cloudflare(url) # => "<html><head>..."
+    import cfscrape
+
+    scraper = cfscrape.create_scraper() # returns a requests.Session object
+    print scraper.get("http://somesite.com").text # => "<!DOCTYPE html><html><head>..."
+
+You use cloudflare-scrape exactly the same way you use Requests. Consult [Requests' documentation](http://docs.python-requests.org/en/latest/user/quickstart/) for more information.
+
+This module is implemented as an adapter, so you can also mount it to an existing requests.Session object if you wish.
+
+    import requests
+    import cfscrape
+
+    sess = requests.session()
+    sess.headers = {"X-Some-Custom-Header": "Some Value"}
+    sess.mount("http://", cfscrape.CloudflareAdapter())
+
+Note that `create_scraper()` is merely a convenience function that creates a new Requests session and mounts the adapter to it. It will also do the mounting for you if you pass it an existing session.
+
+    import requests
+    import cfscrape
+
+    sess = requests.session()
+    sess.headers = {"X-Some-Custom-Header": "Some Value"}
+    sess = cfscrape.create_scraper(sess) # this just runs sess.mount(...)
+
--- cfscrape.py
+++ cfscrape.py
@@ -1,31 +1,74 @@
 import re
-import requests
 import PyV8
+from urlparse import urlparse
+import requests
+from requests.adapters import HTTPAdapter
 
-def grab_cloudflare(url, *args, **kwargs):
-    sess = requests.session()
-    sess.headers["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0"
-    resp = sess.get(url, *args, **kwargs)
-    url = resp.url
-    page = resp.content
+DEFAULT_USER_AGENT = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36")
 
-    if "a = document.getElementById('jschl-answer');" not in page:
-        # Return page as it is; no Cloudflare protection detected
-        return page
+class CloudflareAdapter(HTTPAdapter):
+    def send(self, request, **kwargs):
+        domain = request.url.split("/")[2]
+        resp = super(CloudflareAdapter, self).send(request, **kwargs)
 
-    # Otherwise, Cloudflare anti-bots is on
-    domain = url.split("/")[2]
+        # Check if we already solved a challenge
+        if request._cookies.get("cf_clearance", domain="." + domain):
+            return resp
 
-    challenge = re.search(r'name="jschl_vc" value="(\w+)"', page).group(1)
+        # Check if Cloudflare anti-bot is on
+        if "a = document.getElementById('jschl-answer');" in resp.content:
+            return self.solve_cf_challenge(resp, request.headers, **kwargs)
 
-    builder = re.search(r"setTimeout.+?\r?\n([\s\S]+?a\.value =.+?)\r?\n", page).group(1)
-    builder = re.sub(r"a\.value =(.+?) \+ .+?;", r"\1", builder)
-    builder = re.sub(r"\s{3,}[a-z](?: = |\.).+", "", builder)
+        # Otherwise, no Cloudflare anti-bot detected
+        return resp
 
-    with PyV8.JSContext() as ctxt:
-        # Safely evaluate Javascript expression
-        answer = str(int(ctxt.eval(builder)) + len(domain))
+    def add_headers(self, request):
+        # Spoof Chrome on Linux if no custom User-Agent has been set
+        if "requests" in request.headers["User-Agent"]:
+            request.headers["User-Agent"] = DEFAULT_USER_AGENT
 
-    params = {"jschl_vc": challenge, "jschl_answer": answer}
-    submit_url = "http://%s/cdn-cgi/l/chk_jschl" % domain
-    return sess.get(submit_url, params=params, headers={"Referer": url}, *args, **kwargs).content
+    def solve_cf_challenge(self, resp, headers, **kwargs):
+        headers = headers.copy()
+        url = resp.url
+        parsed = urlparse(url)
+        domain = parsed.netloc
+        page = resp.content
+
+        try:
+            # Extract the arithmetic operation
+            challenge = re.search(r'name="jschl_vc" value="(\w+)"', page).group(1)
+            builder = re.search(r"setTimeout.+?\r?\n([\s\S]+?a\.value =.+?)\r?\n", page).group(1)
+            builder = re.sub(r"a\.value =(.+?) \+ .+?;", r"\1", builder)
+            builder = re.sub(r"\s{3,}[a-z](?: = |\.).+", "", builder)
+
+        except AttributeError:
+            # Something is wrong with the page. This may indicate Cloudflare has changed their
+            # anti-bot technique. If you see this and are running the latest version,
+            # please open a GitHub issue so I can update the code accordingly.
+            raise IOError("Unable to parse Cloudflare anti-bots page. Try upgrading cfscrape, or "
+                          "submit a bug report if you are running the latest version.")
+
+        # Lock must be added explicitly, because PyV8 bypasses the GIL
+        with PyV8.JSLocker():
+            with PyV8.JSContext() as ctxt:
+                # Safely evaluate the Javascript expression
+                answer = str(int(ctxt.eval(builder)) + len(domain))
+
+        params = {"jschl_vc": challenge, "jschl_answer": answer}
+        submit_url = "%s://%s/cdn-cgi/l/chk_jschl" % (parsed.scheme, domain)
+        headers["Referer"] = url
+
+        return requests.get(submit_url, params=params, headers=headers, **kwargs)
+
+
+def create_scraper(session=None):
+    """
+    Convenience function for creating a ready-to-go requests.Session object.
+    You may optionally pass in an existing Session to mount the CloudflareAdapter to it.
+    """
+    sess = session or requests.session()
+    adapter = CloudflareAdapter()
+    sess.mount("http://", adapter)
+    sess.mount("https://", adapter)
+    return sess
