84b0874e0679641dcc3a4828828c2d7dc048d820
Anorov <anorov.vorona@gmail.com>
2016-11-25 13:41:02 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
   name = 'cfscrape',
   packages = ['cfscrape'],
-  version = '1.6.6',
+  version = '1.6.7',
   description = 'A simple Python module to bypass Cloudflare\'s anti-bot page. See https://github.com/Anorov/cloudflare-scrape for more information.',
   author = 'Anorov',
   author_email = 'anorov.vorona@gmail.com',
   url = 'https://github.com/Anorov/cloudflare-scrape',
   keywords = ['cloudflare', 'scraping'],
-  install_requires = ['Js2Py==0.37', 'requests >= 2.0.0']
+  install_requires = ['Js2Py >= 0.37', 'requests >= 2.0.0']
 )
