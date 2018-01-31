428e147cfe8084151077f38cd8cd83dfd9a79f02
Anorov <anorov.vorona@gmail.com>
2018-01-31 14:23:06 -0500

    Add LICENSE and README.md to source distribution

--- cfscrape/__init__.py
+++ cfscrape/__init__.py
@@ -12,7 +12,7 @@ try:
 except ImportError:
     from urllib.parse import urlparse
 
-__version__ = "1.9.2"
+__version__ = "1.9.3"
 
 DEFAULT_USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(
   name = 'cfscrape',
   packages = ['cfscrape'],
-  version = '1.9.2',
+  version = '1.9.3',
   description = 'A simple Python module to bypass Cloudflare\'s anti-bot page. See https://github.com/Anorov/cloudflare-scrape for more information.',
   author = 'Anorov',
   author_email = 'anorov.vorona@gmail.com',
   url = 'https://github.com/Anorov/cloudflare-scrape',
   keywords = ['cloudflare', 'scraping'],
+  include_package_data = True,
   install_requires = ['PyExecJS >= 1.4.0', 'requests >= 2.0.0']
 )
